0edf566dcf3e5ab6e4f0d6704c8c6f395bdb984d
Stefan Broenner <stbrnner@microsoft.com>
2020-01-31 17:04:02 +0100

    Added fanart

--- local_build/build.sh
+++ local_build/build.sh
@@ -10,11 +10,12 @@ cp ../changelog.txt plugin.video.nhkworldtv
 cp ../LICENSE plugin.video.nhkworldtv
 cp ../main.py plugin.video.nhkworldtv
 cp -r ../resources plugin.video.nhkworldtv
+cp -r ../lib plugin.video.nhkworldtv
 7za a -tzip plugin.video.nhkworldtv-0.0.4.zip plugin.video.nhkworldtv
 
 cp ../addon.xml ../../zips/plugin.video.nhkworldtv/
-cp ../changelog.txt ../../zips/plugin.video.nhkworldtv/
 cp ../resources/icon.png ../../zips/plugin.video.nhkworldtv/
+cp ../resources/fanart.jpg ../../zips/plugin.video.nhkworldtv/
 
 cp -r *.zip ../../zips/plugin.video.nhkworldtv/
 rm -rf plugin.video.nhkworldtv
