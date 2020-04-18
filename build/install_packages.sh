c369dff81987f94aa2fcffcd409d267739e6ff68
Stefan Broenner <sbroenne@Stefans-MBP.fritz.box>
2020-04-18 09:55:47 +0200

    UnitTest BugFix

--- build/install_packages.sh
+++ build/install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install packackes thar are not in PyPi. This only needs to be done once.
-# export PYTHON_VERSION=2.7
+#export PYTHON_VERSION=2.7
 echo "PYTHON VERSION: $PYTHON_VERSION"
 rm -rf prereq
 mkdir prereq
@@ -17,4 +17,6 @@ rm -rf kodi.six-master
 
 # Install routing.py
 cd .. 
-cp routing/routing.py $(pipenv --venv)/lib/python$PYTHON_VERSION/site-packages
+export SITE_PACKAGES="$(pipenv --venv)/lib/python$PYTHON_VERSION/site-packages/"
+ECHO "Site Packages Direcorty: $SITE_PACKAGES"
+cp routing/routing.py $SITE_PACKAGES
