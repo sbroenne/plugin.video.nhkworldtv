d1ad02a065ada3dba3b08843bdbeed2253ec9b61
sbroenne <stefan_broenner@yahoo.com>
2023-10-29 01:33:27 +0200

    Pushing a .py change to re-enable actions

--- plugin.video.nhkworldtv/tests/test_lib_vod.py
+++ plugin.video.nhkworldtv/tests/test_lib_vod.py
@@ -1,5 +1,4 @@
 import pytest
-
 from lib import vod
 from lib.episode import Episode
 
@@ -22,12 +21,9 @@ def test_get_episode_from_episode_list():
 
 
 def test_get_episode_from_cache_720p(test_episode):
-
     return_value = vod.get_episode_from_cache(test_episode, use_720p=True)
     path = return_value[0]
     assert "http://" in path
-
-
 
 
 def test_get_episode_not_in_cache():
@@ -37,9 +33,7 @@ def test_get_episode_not_in_cache():
 
 
 def test_resolve_episode_720p(test_episode):
-
     assert isinstance(
         vod.resolve_vod_episode(test_episode.vod_id, use_720p=True), Episode
     )
     assert test_episode.is_playable is True
-
